9ae0cb5083c6575a4b7bbf72b8c1dced80dbd4a5
Konstantin Timofeev <39969686+famone@users.noreply.github.com>
2025-09-01 14:07:03 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="h-screen py-4">
+  <div class="sm:w-md mx-auto">
+    <div class="text-white flex justify-center items-center gap-2">
+      <span class="size-2 bg-emerald-400 rounded-full block"></span>
+      48 онлайн
+    </div>
+  </div>
+  <BottomMenu />
+ </div>
+</template>
+
+<script setup lang="ts">
+import BottomMenu from './components/BottomMenu.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BottomMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-black fixed bottom-0 left-0 py-3 pb-4 flex justify-center items-center gap-10 w-full">
+    <div class="text-white text-center">
+      <Gamepad2 class="size-6 mx-auto" />
+      <p class="text-sm">PvP</p>
+    </div>
+    <div class="text-neutral-500 text-center">
+      <Gift class="size-6 mx-auto" />
+      <p class="text-sm">Gifts</p>
+    </div>
+    <div class="text-neutral-500  text-center">
+      <User class="size-6 mx-auto" />
+      <p class="text-sm">Profile</p>
+    </div>
+    <div class="text-neutral-500  text-center">
+      <Store class="size-6 mx-auto" />
+      <p class="text-sm">Shop</p>
+    </div>
+    <div class="text-neutral-500  text-center">
+      <Sparkles class="size-6 mx-auto" />
+      <p class="text-sm">Tasks</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Gamepad2, Gift, Sparkles, Store, User } from 'lucide-vue-next';
+
+
+</script>
+
+<style scoped>
+
+</style>
